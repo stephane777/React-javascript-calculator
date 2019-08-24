eb697f7f56263168090c46e0d1ad7c92d124778c
Stephane Candelas <stephanecandelas@gmail.com>
2019-08-24 20:09:57 +0100

    correction bug on regex and compute new operator after a compute

--- src/js/Calculator.js
+++ src/js/Calculator.js
@@ -5,118 +5,146 @@ import labels from './calculator_labels';
 import Buttons from './Buttons';
 
 class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      display: '27',
-      formula: '43'
-    };
-    this.handleClickButton = this.handleClickButton.bind(this);
-    this.resetDisplay = this.resetDisplay.bind(this);
-    this.digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-    this.operand = ['/', 'x', '-', '+', '*'];
-  }
-  componentDidMount() {
-    // const buttons = document.querySelector('.buttons')
-    // Array.from(buttons.childNodes).map(button=>{
-    //     button.addEventListener('click',)
-    // })
-  }
-  resetDisplay() {
-    this.setState({
-      display: '0',
-      formula: ''
-    });
-  }
-  handleClickButton(e) {
-    let buttonClicked = e.target.innerText;
-    const isDigitClicked = this.digits.includes(buttonClicked);
-    const isOperatorClicked = this.operand.includes(buttonClicked);
-    const isDisplayANumber = !isNaN(this.state.display);
-    const isDisplayAnOperand = this.operand.includes(this.state.display);
-    const isDecimal = this.state.display.includes('.');
+     constructor(props) {
+          super(props);
+          this.state = {
+               display: '0',
+               formula: ''
+          };
+          this.handleClickButton = this.handleClickButton.bind(this);
+          this.resetDisplay = this.resetDisplay.bind(this);
+          this.digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+          this.operand = ['/', 'x', '-', '+', '*'];
+     }
+     componentDidMount() {
+          // const buttons = document.querySelector('.buttons')
+          // Array.from(buttons.childNodes).map(button=>{
+          //     button.addEventListener('click',)
+          // })
+     }
+     resetDisplay() {
+          this.setState({
+               display: '0',
+               formula: ''
+          });
+     }
+     handleClickButton(e) {
+          let buttonClicked = e.target.innerText;
+          const isDigitClicked = this.digits.includes(buttonClicked);
+          const isOperatorClicked = this.operand.includes(buttonClicked);
+          const isDisplayANumber = !isNaN(this.state.display);
+          const isDisplayAnOperand = this.operand.includes(this.state.display);
+          const isDecimal = this.state.display.includes('.');
 
-    buttonClicked === 'AC' ? this.resetDisplay() : '';
-    // console.log(`typeof buttonClicked: ${typeof buttonClicked}`)
-    if (isDisplayANumber && isDigitClicked) {
-      console.log(
-        `isDisplayANumber:${isDisplayANumber} isDigitalClicked:${isDigitClicked}`
-      );
+          if (buttonClicked === 'AC') {
+               this.resetDisplay();
+               return;
+          }
 
-      if (!isDecimal || (isDecimal && buttonClicked != '.')) {
-        this.setState(state => {
-          return state.display === '0' && buttonClicked === '.'
-            ? {
-                display: state.display + buttonClicked,
-                formula: state.display + buttonClicked
-              }
-            : {
-                display:
-                  state.display === '0'
-                    ? buttonClicked
-                    : state.display + buttonClicked,
-                formula:
-                  state.formula === '0'
-                    ? buttonClicked
-                    : state.formula + buttonClicked
-              };
-        });
-      }
+          // console.log(`typeof buttonClicked: ${typeof buttonClicked}`)
+          if (isDisplayANumber && isDigitClicked) {
+               console.log(
+                    `isDisplayANumber:${isDisplayANumber} isDigitalClicked:${isDigitClicked}`
+               );
 
-      //  concat the new digit in display : "234" + "5" => "2345"
-    }
-    // if  (isDisplayAnOperand && buttonClicked ==='.'){
-    //     this.setState(state=>({
-    //         display: state.display+'0.',
-    //         formula: state.formula + '0.'
-    //     }))
-    // }
-    if (isDisplayANumber && isOperatorClicked) {
-      buttonClicked = buttonClicked === 'x' ? '*' : buttonClicked;
-      this.setState(state => ({
-        display: buttonClicked,
-        formula: state.formula + buttonClicked
-      }));
-    }
-    if (isDisplayAnOperand) {
-      this.setState(state => {
-        return buttonClicked === '.'
-          ? {
-              display: '0.',
-              formula: state.formula + '0.'
-            }
-          : {
-              display: buttonClicked,
-              formula: state.formula + buttonClicked
-            };
-      });
-    }
-    if (buttonClicked === '=') {
-      const regexp = new RegExp(/\+|\-|\*|\//g);
-      // const regexp2 = new RegExp(/[-+*/]?\d+/g)
-      // console.log(this.state.formula.split(regexp))
+               if (!isDecimal || (isDecimal && buttonClicked != '.')) {
+                    this.setState(state => {
+                         return state.display === '0' && buttonClicked === '.'
+                              ? {
+                                     display: state.display + buttonClicked,
+                                     formula: state.display + buttonClicked
+                                }
+                              : {
+                                     display:
+                                          state.display === '0'
+                                               ? buttonClicked
+                                               : state.display + buttonClicked,
+                                     formula:
+                                          state.formula === '0'
+                                               ? buttonClicked
+                                               : state.formula + buttonClicked
+                                };
+                    });
+               }
 
-      // "34+-*54-/*34"
+               //  concat the new digit in display : "234" + "5" => "2345"
+          }
 
-      if (regexp.test(this.state.formula)) {
-        // window.open('https://www.google.com/search?q=sexy+italian+men')
-        this.setState(state => ({
-          display: String(eval(state.formula)),
-          formula: state.formula + buttonClicked + String(eval(state.formula))
-        }));
-      } else {
-        console.log("doesn't match anything!");
-      }
-    }
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <Output display={this.state.display} formula={this.state.formula} />
-        <Buttons labels={labels} clickButton={this.handleClickButton} />
-      </React.Fragment>
-    );
-  }
+          if (isDisplayANumber && isOperatorClicked) {
+               buttonClicked = buttonClicked === 'x' ? '*' : buttonClicked;
+               const isComputed = this.state.formula.includes('=');
+               if (isComputed) {
+                    this.setState(state => ({
+                         display: buttonClicked,
+                         formula: state.display + buttonClicked
+                    }));
+               } else {
+                    this.setState(state => ({
+                         display: buttonClicked,
+                         formula: state.formula + buttonClicked
+                    }));
+               }
+          }
+          if (isDisplayAnOperand) {
+               buttonClicked = buttonClicked === 'x' ? '*' : buttonClicked;
+               this.setState(state => {
+                    return buttonClicked === '.'
+                         ? {
+                                display: '0.',
+                                formula: state.formula + '0.'
+                           }
+                         : {
+                                display: buttonClicked,
+                                formula: state.formula + buttonClicked
+                           };
+               });
+          }
+          if (buttonClicked === '=') {
+               const regexp = new RegExp(/\+|\-|\*|\//g);
+               const regexp2 = new RegExp(/[-+*/]?-?\d+(\.\d+)?/g);
+               // console.log(this.state.formula.split(regexp));
+               console.log(this.state.formula.match(regexp2));
+               // "34+-*54-/*34"
+               const cleanedFormula = this.state.formula.match(regexp2)
+                    ? this.state.formula.match(regexp2).join('')
+                    : this.state.formula;
+               const isCleaned = cleanedFormula != this.state.formula;
+
+               console.log(isCleaned);
+               console.log(`cleanedFormula: ${cleanedFormula}`);
+               console.log(`this.state.formula: ${this.state.formula}`);
+
+               if (regexp.test(this.state.formula)) {
+                    this.setState(state => ({
+                         display: isCleaned
+                              ? String(eval(cleanedFormula))
+                              : String(eval(state.formula)),
+                         formula:
+                              cleanedFormula +
+                              buttonClicked +
+                              (isCleaned
+                                   ? String(eval(cleanedFormula))
+                                   : String(eval(state.formula)))
+                    }));
+               } else {
+                    console.log("doesn't match anything!");
+               }
+          }
+     }
+     render() {
+          return (
+               <React.Fragment>
+                    <Output
+                         display={this.state.display}
+                         formula={this.state.formula}
+                    />
+                    <Buttons
+                         labels={labels}
+                         clickButton={this.handleClickButton}
+                    />
+               </React.Fragment>
+          );
+     }
 }
 
 export default Calculator;
